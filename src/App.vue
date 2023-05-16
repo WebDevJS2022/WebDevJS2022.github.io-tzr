22eaa29817be0afbda8c65daedde7f005ad413a9
WeDev <webdevjs2022@gmail.com>
2023-05-16 08:40:46 +0500

    Создание компонента NotFoundPage

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <MainPage v-if="currentPage === 'main'" />
   <ProductPage v-else-if="currentPage === 'product'" />
+  <NotFoundPage v-else/>
 </template>
 
 <script>
 import MainPage from '@/pages/MainPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
+import NotFoundPage from '@/pages/NotFoundPage.vue';
 
 export default {
   name: 'App',
-  components: { MainPage, ProductPage },
+  components: { MainPage, ProductPage, NotFoundPage },
   data() {
     return {
       currentPage: 'main',
