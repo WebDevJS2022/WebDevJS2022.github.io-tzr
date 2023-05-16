4a81e573d6cd423521d29f35186e1a600676b09c
WeDev <webdevjs2022@gmail.com>
2023-05-16 18:35:40 +0500

    6.2 Добавление Корзины

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import MainPage from '@/pages/MainPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
+import CartPage from '@/pages/CartPage.vue';
 
 // сообщаем Vue что собираемся использовать Vue-router
 Vue.use(VueRouter);
@@ -11,6 +12,7 @@ Vue.use(VueRouter);
 const routes = [
   { name: 'main', component: MainPage, path: '/' },
   { name: 'product', component: ProductPage, path: '/product/:id' },
+  { name: 'cart', component: CartPage, path: '/cart' },
   { name: 'notFound', component: NotFoundPage, path: '*' },
 ];
 
