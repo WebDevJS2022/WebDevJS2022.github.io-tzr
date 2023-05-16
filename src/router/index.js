f7587fd566d537fa1f3fb211b8c2eb6f922043a4
WeDev <webdevjs2022@gmail.com>
2023-05-16 09:41:29 +0500

    6.1 Vue-роутер - Установка

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// сообщаем Vue что собираемся использовать Vue-router
+Vue.use(VueRouter);
+
+// создаем новый экземпляр роутера
+const router = new VueRouter();
+
+export default router;
