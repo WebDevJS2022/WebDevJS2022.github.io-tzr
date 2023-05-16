6dd382445f7ded84daf563950831eb667df3cad9
WeDev <webdevjs2022@gmail.com>
2023-05-16 08:43:31 +0500

    Упрощение работы с компонентами

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <MainPage v-if="currentPage === 'main'" />
-  <ProductPage v-else-if="currentPage === 'product'" />
-  <NotFoundPage v-else/>
+    <component :is="currentPageComponent" />
 </template>
 
 <script>
 import MainPage from '@/pages/MainPage.vue';
 import ProductPage from '@/pages/ProductPage.vue';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
+
+const routes = {
+  main: 'MainPage',
+  product: 'ProductPage',
+};
 
 export default {
   name: 'App',
@@ -16,6 +19,11 @@ export default {
     return {
       currentPage: 'main',
     };
+  },
+  computed: {
+    currentPageComponent() {
+      return routes[this.currentPage] || 'NotFoundPage';
+    },
   },
 };
 </script>
