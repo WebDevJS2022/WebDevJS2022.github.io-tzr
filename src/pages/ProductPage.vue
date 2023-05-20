bbc76925830a179017cfe8c73c82de6ceafe2d24
WeDev <webdevjs2022@gmail.com>
2023-05-20 09:37:47 +0500

    7.6 Правильный переход через адресную строку

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -31,26 +31,30 @@
           <ul class="pics__list">
             <li class="pics__item">
               <a href="" class="pics__link pics__link--current">
-                <img width="98" height="98" src="img/phone-square-1.jpg"
-                srcset="img/phone-square-1@2x.jpg 2x" alt="Название товара">
+                <img width="98" height="98"
+                :src="product.preview.file.url"
+                :alt="product.title">
               </a>
             </li>
             <li class="pics__item">
               <a href="" class="pics__link">
-                <img width="98" height="98" src="img/phone-square-2.jpg"
-                srcset="img/phone-square-2@2x.jpg 2x" alt="Название товара">
+                <img width="98" height="98"
+                :src="product.preview.file.url"
+                :alt="product.title">
               </a>
             </li>
             <li class="pics__item">
               <a href="" class="pics__link">
-                <img width="98" height="98" src="img/phone-square-3.jpg"
-                srcset="img/phone-square-3@2x.jpg 2x" alt="Название товара">
+                <img width="98" height="98"
+                :src="product.preview.file.url"
+                :alt="product.title">
               </a>
             </li>
             <li class="pics__item">
               <a class="pics__link" href="#">
-                <img width="98" height="98" src="img/phone-square-4.jpg"
-                srcset="img/phone-square-4@2x.jpg 2x" alt="Название товара">
+                <img width="98" height="98"
+                :src="product.preview.file.url"
+                :alt="product.title">
               </a>
             </li>
           </ul>
@@ -212,6 +216,11 @@ export default {
   },
   created() {
     this.loadProduct();
+  },
+  watch: {
+    '$route.params.id': function () {
+      this.loadProduct();
+    },
   },
 };
 </script>
